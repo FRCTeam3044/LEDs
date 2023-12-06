1e75e703d691dd19201ad38a163d040b3cdce352
ghorrocks217 <76536843+ghorrocks217@users.noreply.github.com>
2023-12-05 20:19:54 -0500

    Made Package, Started Blinkin Development.

--- src/main/java/team/frcteam3044/LEDController.java
+++ src/main/java/team/frcteam3044/LEDController.java
@@ -1,10 +1,8 @@
 package team.frcteam3044;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
-/**
- * This is a test commit
- */
 public class LEDController {
     /**
      * Defines the blinkin module for the colors.
@@ -19,49 +17,75 @@ public class LEDController {
      */
     double blinkColorOne = 0.0;
     /**
+     * Save of time2 from twoColorBlinkPattern.
+     */
+    double colorTwoBlinkTime = 0.0;
+    /**
      * Save of color2 from twoColorBlinkPattern.
      */
     double blinkColorTwo = 0.0;
     /**
-     * Save of time2 from twoColorBlinkPattern.
+     * Times the LEDs.
      */
-    double colorTwoBlinkTime = 0.0;
-    
-
+    Timer timer = new Timer();
+    /**
+     * Sets it to one color.
+     */
+    double solidColor = 0.0;
+    /**
+     * True = blinking,
+     * False = solid.
+     */
+    boolean solidOrBlink = false;
 
     /**
      * Turn on the LEDs.
+     * 
      * @param id - Set this to your id of your blinkin module.
      */
     public LEDController(int id) {
-        
+        blinkin = new Spark(id);
     }
 
     /**
-     * Make pattern for the LEDs! :)
-     * @param time1 - The amount of time the color1 stays up.
-     * @param color1 - The first color in the pattern. :P
-     * @param time2 - The amount of time color2 stays up.
-     * @param color2 - The second color in the pattern. XD
+     * Make pattern for the LEDs!
+     * 
+     * @param time1  - The amount of time the color1 stays up.
+     * @param color1 - The first color in the pattern.
+     * @param time2  - The amount of time color2 stays up.
+     * @param color2 - The second color in the pattern.
      */
     public void twoColorBlinkPattern(double time1, double color1, double time2, double color2) {
-
+        solidOrBlink = true;
+        colorOneBlinkTime = time1;
+        colorTwoBlinkTime = time2;
+        blinkColorOne = color1;
+        blinkColorTwo = color2;
     }
-    
+
     /**
-     * Checking how much time has passed to set the colors. <3
+     * Checking how much time has passed to set the colors.
      */
     public void updatePeriodic() {
-        
+        if (solidOrBlink == true) {
+            // Blinking pattern.
+            // blinkin.set()
+        } else {
+            // Solid Colour
+
+        }
     }
 
     /**
      * Controls the color of the LEDs.
+     * 
      * @param colorValue - 0-1 because it works like a motor.
-     * Use specific number corresponding to the color you desire. Interesting lol
+     *                   Use specific number corresponding to the color you desire.
      */
     public void colorChange(double colorValue) {
+        solidOrBlink = false;
 
+        solidColor = colorValue;
     }
 
 }
